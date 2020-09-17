9332561a5c91a1a8e0ee53063a98c73ab4d3b50c
Shota Kamei <kameishota1023@gmail.com>
2020-09-17 11:37:19 +0900

    テーブル作成

---
+++ app/database/migrations/2020_09_17_023403_create_contentitems_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentitemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contentitems', function (Blueprint $table) {
+            $table->id();
+            $table->softDeletes();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('content_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contentitems');
+    }
+}
