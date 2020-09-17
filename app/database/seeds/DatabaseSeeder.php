96e174bcf6b0ffc401cc962609c8b44f288ad3b1
Shota Kamei <kameishota1023@gmail.com>
2020-09-17 14:52:59 +0900

    seeder完了

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // $this->call(usersTableSeeder::class);
+        // $this->call(contentsTableSeeder::class);
+        $this->call(contentitemsTableSeeder::class);
+
     }
 }
