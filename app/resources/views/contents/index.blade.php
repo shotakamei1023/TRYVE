b3b06da3ba4cec331de1d9f47e9835595964b3c0
Shota Kamei <kameishota1023@gmail.com>
2020-10-22 14:20:08 +0900

    検索画面、管理画面修正終了

--- app/resources/views/contents/index.blade.php
+++ app/resources/views/contents/index.blade.php
@@ -1,35 +1,56 @@
 @extends('layouts.app')
 
 @section('search')
-<h2>代行申請一覧</h2>
+<h3>依頼検索画面</h3>
 <form action="/contents/find" method="post">
 @csrf
-<input type="text" name="title" value="{{$title}}" placeholder="タイトル">
-<input type="text" name="prefectures" value="{{$prefectures}}" placeholder="都道府県">
-<input type="number" name="price" value="{{$price}}" placeholder="報酬">
-<input type="submit" value="検索"> 
+        <div class="form-row">
+                <div class="col">
+                        <input type="text" class="form-control" name="title" value="{{$title}}" placeholder="タイトル">
+                </div>
+                <div class="col">
+                        <input type="text" class="form-control" name="prefectures" value="{{$prefectures}}" placeholder="都道府県">
+                </div>
+                <div class="col">
+                        <input type="number" class="form-control" name="price" value="{{$price}}" placeholder="報酬">
+                </div>
+                <input class='btn btn-secondary' type="submit" value="検索"> 
+        </div>
 </form>
 @endsection
 
 @section('content')
 @if (session('flash_message'))
-        <div class="flash_message">
+        <div class="flash_message alert alert-warning">
                 {{ session('flash_message') }}
         </div>
 @endif
-@foreach($contents as $content)
-<table>
-<tr>
-        <td>{{$content->title}}</td>
+<table class="table table-hover">
+  <thead class="thead-light">
+    <tr>
+        <th scope="col">依頼タイトル</th>
+        <th scope="col">都道府県</th>
+        <th scope="col">報酬</th>
+        <th scope="col">作成者</th>
+        <th scope="col">申請者人数</th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+        @foreach($contents as $content)
+    <tr>
+        <th scope="row">{{$content->title}}</th>
         <td>{{$content->prefectures}}</td>
         <td>{{$content->price}}</td>
         <td>{{$content->owner->name}}</td>
-        <td>{{DB::table('content_items')->where('content_id','=',$content->id)->whereNull('deleted_at')->count()}}</td>
+        <td >{{DB::table('content_items')->where('content_id','=',$content->id)->whereNull('deleted_at')->count()}}人</td>
         <td><button type="button" class="btn btn-info" ><a href="{{ route('content.show', ['id' => $content->id]) }}"><font color=white>詳細</font></a></button></td>
-        <td><form method="post" action="{{ route('content.post', ['id' => $content->id]) }}">@method('PATCH')@csrf<input type="submit" value="代行申請" ></form></td>
-<tr>
+        <td><form method="post" action="{{ route('content.post', ['id' => $content->id]) }}">@method('PATCH')@csrf<input class="btn btn-primary" type="submit" value="代行申請" ></form></td>
+    </tr>
+        @endforeach
+  </tbody>
 </table>
-@endforeach
 <div class="d-flex justify-content-center">
     {{ $contents->links() }}
 </div>
@@ -38,3 +59,4 @@
 @section('create')
 <button type="button" class="btn btn-success"><a href="{{ action('ContentsController@create') }}"><font color=white>依頼作成</font></a></button>
 @endsection
+
--- app/resources/views/mycontents/index.blade.php
+++ app/resources/views/mycontents/index.blade.php
@@ -1,33 +1,42 @@
 @extends('layouts.app')
 
 @section('search')
-<h2>依頼作成画面</h2>
+<h3>依頼作成画面</h3>
 <form action="/mypage/contents/find" method="post">
 @csrf
-<div class="form-group">
-  <label for="select1a">ステータスを入力することで絞り込むことができます。</label>
-  <select name="content_status" placeholder=""　id="select1a" class="form-control">
-        <option value="1">受付中</option>
-        <option value="2">申請が届きました</option>
-        <option value="3">代行依頼中</option>
-        <option value="4">依頼完了</option>
-  </select>
-</div>
-<input type="submit" value="検索"> 
+        <div class="form-row">
+                <div class="col">
+                        <select name="content_status" placeholder=""　id="select1a" class="form-control">
+                        <option value="1">受付中</option>
+                        <option value="2">申請が届きました</option>
+                        <option value="3">代行依頼中</option>
+                        <option value="4">依頼完了</option>
+                        </select>
+                </div>
+        <input class='btn btn-secondary' type="submit" value="検索"> 
+        </div>
 </form>
 @endsection
 
 @section('content')
 @foreach($contents as $content)
-<table>
-<tr>
-        <td>{{$content->id}}</td>
-        <td>{{$content->title}}</td>
+<table class="table table-hover">
+  <thead class="thead-light">
+    <tr>
+        <th scope="col">依頼タイトル</th>
+        <th scope="col">ステータス</th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+        @foreach($contents as $content)
+    <tr>
+        <th scope="row"><a href="{{ route('content.show', ['id' => $content->id]) }}"><font color=black>{{$content->title}}</font></a></th>
         <td>{{$content->content_text}}</td>
         @if ($content->content_status == 1 )
-                <td>{{DB::table('content_items')->where('content_id','=',$content->id)->count()}}人デート代行を申請しています</td>
                 <td><button type="button" class="btn btn-secondary"><a href="{{ route('mycontent.edit', ['id' => $content->id]) }}"><font color=white>編集</font></a></button></td>
-                <td><form method="post" action="{{ route('mycontent.destroy', ['id' => $content->id]) }}">@method('DELETE')@csrf<input type="submit" value="削除" ></form></td>
+                <td><form method="post" action="{{ route('mycontent.destroy', ['id' => $content->id]) }}">@method('DELETE')@csrf<input class='btn btn-danger' type="submit" value="削除" ></form></td>
         @elseif ($content->content_status == 2)
                 <td><button type="button" class="btn btn-info"><a href="{{ route('mycontent.load', ['id' => $content->id]) }}"><font color=white>申請者リストを見る</font></a></button></td>
                 <td></td>
@@ -43,8 +52,9 @@
                         <td><button type="button" class="btn btn-primary"><a href="{{ route('mycontent.revue', ['id' => $content->id]) }}"><font color=white>レポートを評価する</font></a></button></td>     
                 @endif 
         @endif
-
-<tr>
+    </tr>
+        @endforeach
+  </tbody>
 </table>
 @endforeach
 <div class="d-flex justify-content-center">
